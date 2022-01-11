203c1981de261a20f5bc41f9bd050804856f31d5
Ronald van der Kooij <ronald@sharpsolutions.nl>
2022-01-11 22:12:17 +0100

    vite build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 const vuePlugin = require('@vitejs/plugin-vue')
-
+const path = require('path');
 /**
  * @type {import('vite').UserConfig}
  */
@@ -11,6 +11,22 @@ module.exports = {
     }
   ],
   build: {
-    minify: false
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/vue-hybrid-scripts/useHybridScript.ts'),
+      name: 'vue3-hybrid-scripts',
+      fileName: (format) => `vue3-hybrid-scripts.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
   }
 }
